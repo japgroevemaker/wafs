d88d7a48893861fac8e0de2ae7b7877aa2328402
Joep Gravemaker <joepgravemaker@MacBook-Pro-van-Joep.local>
2018-02-21 15:31:49 +0100

    add @japgroevemaker

--- Opdracht 5 - SPA/static/js/api.js
+++ Opdracht 5 - SPA/static/js/api.js
@@ -1,14 +1,101 @@
-var request = new XMLHttpRequest();
+console.log('global scope');
+//create local scope
+(function() {
+  console.log('local scope');
+  //initialize application
+  var app = {
+    // is een method wat je kan uitvoeren
+    init: function() {
+      console.log('app initialised')
+      api.getData()
+    }
+  }
+  //handle routes & state
+
+  var api = {
+    getData: function() {
+      var request = new XMLHttpRequest();
 
 
-request.onload = function() {
-    var data = JSON.parse(request.responseText);
-    console.log(data)
-};
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          var data = JSON.parse(request.responseText);
 
-request.onError = function() {
+          template.render(data)
+        } else {
 
-}
+        }
+      };
 
-request.open('GET', 'http://api.weatherunlocked.com/api/forecast/nl.1972?lang=nl&app_id=b7dff351&app_key=a82cccc7ee0981db93b7508449f13e0b', true);
-request.send();
+      request.onError = function() {
+
+      }
+
+      request.open('GET', 'http://api.weatherunlocked.com/api/forecast/nl.1972?lang=nl&app_id=b7dff351&app_key=a82cccc7ee0981db93b7508449f13e0b', true);
+      request.setRequestHeader("Accept", "application/json");
+      request.send();
+
+      routes.init()
+
+    }
+  }
+
+  var routes = {
+
+    init: function() {
+      //hookup router
+        routie({
+          'start': function(){
+            var targetElement = document.getElementById(this.path);
+            template.toggle.targetElement;
+          },
+          'weather': function(){
+            var targetElement = document.getElementById(this.path);
+            template.toggle.targetElement;
+          },
+          'weatherDetail': function(){
+            var targetElement = document.getElementById(this.path);
+            template.toggle.targetElement;
+          }
+
+          })
+      // what's in the hash?
+      window.addEventListener("hashchange", function(event) {
+        var route = location.hash;
+        template.toggle(route)
+      })
+    }
+  }
+
+
+
+  //render / toggle sections
+  var template = {
+    render: function(data) {
+      // hookup template engine
+
+      var huidig = function(){
+        return data.this
+      }
+
+      console.log(data);
+      Transparency.render(document.getElementById('listItems'), data.Days);
+      Transparency.render(document.getElementById('listItemDetail'), data.Days);
+    },
+
+
+    toggle: function(route) {
+      console.log(route);
+      var sections = document.querySelectorAll("section");
+      var section = document.querySelector(route);
+
+      for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.add("none")
+      }
+
+      section.classList.remove("none")
+    }
+  }
+  //start the application
+  app.init()
+})()
--- Opdracht 5 - SPA/static/js/app.js
+++ Opdracht 5 - SPA/static/js/app.js
@@ -13,63 +13,156 @@ console.log('global scope');
   //handle routes & state
 
   var api = {
+    response: {},
     getData: function() {
       var request = new XMLHttpRequest();
 
+
       request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
-          var data = JSON.parse(request.responseText);
-          template.render(data)
+          api.response = JSON.parse(request.responseText);
+          routes.init(api.response.results)
+
         } else {
 
         }
       };
 
       request.onError = function() {
-
+        console.log('het werkt niet')
       }
 
-      request.open('GET', 'http://api.weatherunlocked.com/api/forecast/nl.1972?lang=nl&app_id=b7dff351&app_key=a82cccc7ee0981db93b7508449f13e0b', true);
+      request.open('GET', 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=3d8eafd7eaf04aa6a1493eaa050714a7', true);
+      request.setRequestHeader("Accept", "application/json");
       request.send();
 
-      routes.init()
+
 
     }
   }
+
   var routes = {
 
-    init: function() {
+    init: function(data) {
       //hookup router
-        routie({
-          'start': function(){
-            var targetElement = document.getElementById(this.path);
-            template.toggle.targetElement;
-          },
-          'weather': function(){
-            var targetElement = document.getElementById(this.path);
-            template.toggle.targetElement;
-          }
+      routie({
+        'start': function() {
+          console.log('test')
+          template.toggle('#Start')
+        },
+        'movies': function() {
+          template.toggle('#movies')
+          template.render(data)
+        },
+        'movies/:name': function(name) {
+          console.log(name)
+          template.toggle('#moviemain')
+          template.movieDetail(data, name)
 
-        })
-      // what's in the hash?
-      window.addEventListener("hashchange", function(event) {
-        var route = location.hash;
-        template.toggle(route)
+          // template.render(name);
+
+          // var names = name
+
+        }
       })
+      // what's in the hash?
+      // window.addEventListener("hashchange", function(event) {
+      //   var route = location.hash;
+      //   template.toggle(route)
+      // })
     }
   }
-
-
 
   //render / toggle sections
   var template = {
     render: function(data) {
       // hookup template engine
-          var listItems = [
-            {date: data}
-          ]
-      console.log(data);
-      Transparency.render(document.getElementById('listItems'), listItems);
+
+      var dataFilm = data.map(function(i) { //Map function thanks to Keving Wang and Oy
+        return {
+          byline: i.byline,
+          critics_pick: i.critics_pick,
+          date_updated: i.date_updated,
+          title: i.display_title,
+          display_link: i.display_title.replace(/ /g, "_"),
+          headline: i.headline,
+          multimedia: i.multimedia.src,
+          link: i.link.url,
+          summary: i.summary_short
+        }
+      });
+
+      var movies = {
+        display_title: {
+          href: function(params) {
+            return `#movies/${this.display_link}`
+          }
+        },
+        headline: {
+          id: function(params) {
+            return this.headline
+          }
+        },
+        image: {
+          src: function(params) {
+            return this.multimedia
+          }
+        },
+        link: {
+          id: function(params) {
+            return this.link
+          }
+        },
+        summary: {
+          id: function(params) {
+            return this.summary
+          }
+        }
+      };
+
+      console.log(movies)
+
+      var target = document.getElementById('activities');
+      console.log(api.response.results);
+
+      Transparency.render(target, dataFilm, movies);
+    },
+
+    movieDetail: function(data, name) {
+      var spaceName = name.replace(/_/g, " ")
+
+      var dataFilm2 = data.filter(function(i) {
+        return i.display_title == spaceName
+      }).map(function(i) {
+        console.log(i)
+        return {
+          display_title: i.display_title,
+          multimedia: i.multimedia.src,
+          summary: i.link.type
+        }
+      });
+
+      var movieDetail = {
+        Title: {
+          href: function(params) {
+            return this.display_title
+          }
+        },
+        image: {
+          src: function(params) {
+            return this.multimedia
+          }
+        },
+        summary: {
+          id: function(params){
+            return this.article
+          }
+        }
+      }
+      var target2 = document.getElementById('moviemain');
+
+      Transparency.render(target2, dataFilm2, movieDetail);
+
     },
 
 
@@ -81,8 +174,13 @@ console.log('global scope');
       for (var i = 0; i < sections.length; i++) {
         sections[i].classList.add("none")
       }
+      if (document.querySelector(route)) {
+        document.querySelector(route).classList.remove("none")
+      } else {
+        return
+      }
 
-      section.classList.remove("none")
+      // section.classList.remove("none")
     }
   }
   //start the application
